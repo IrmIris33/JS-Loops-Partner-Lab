e8f2a801c6bc3f485e8c99c903d01f0cba132330
Gregory Lowe <gregory.g.lowe@gmail.com>
2020-11-19 17:04:29 -0500

    almost finished second problem

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ for (let i = 10; i <=300; i += 10){
     console.log((i), + (i * 1.094).toFixed(2));
 }
 
+let i = 0
+let numArr = []
+
+for (let i = 1; i <= 10; i++){
+    let num = prompt("please enter number");
+
+    numArr.push(num);
+
+}
+
+console.log(numArr);
+
+alert(Math.max(numArr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
