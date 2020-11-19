2e8f2ff32ad5a880b3801cf5f1bf7aa8de1c48c3
IrmIris33 <irmirisortizortiz@outlook.com>
2020-11-19 16:31:43 -0500

    Solved problem 1

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 
-console.log("Celcius" + "Fahrenheit");
+//console.log("Celcius" + "Fahrenheit");
 
-for (let i = 0; i <=100; i++){
-    console.log((i),(i * 9/5 +32));
+//for (let i = 0; i <=100; i++){
+//    console.log((i), (i * 9/5 +32));
+//}
+
+
+console.log("Meters " + " Yards");
+
+for (let i = 10; i <=300; i += 10){
+    console.log((i), + (i * 1.094).toFixed(2));
 }
 
